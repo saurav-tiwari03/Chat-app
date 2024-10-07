dae0433b814ab86bf2a60f5e06ead79a8174c282
saurav-tiwari03 <sauravtiwari5625@gmail.com>
2024-10-07 22:30:44 +0530

    final push for prodyction

--- backend/index.js
+++ backend/index.js
@@ -27,3 +27,7 @@ const PORT = process.env.PORT || 5000
 server.listen(PORT,() => {
   console.log(`Server is running on port ${PORT}`);
 })
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+})
